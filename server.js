3a8192c42c922609e497bf0f5184292164eaff6c
EricChukoltyansky <kazohelmet@gmail.com>
2023-03-19 11:09:52 +0200

    deploy

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.get("/api", (req, res) => {
   res.json({ users: ["John", "Paul", "George", "Ringo"] });
