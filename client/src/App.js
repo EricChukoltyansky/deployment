63a6306111eb663aa6f34b0a173688116b987bb4
EricChukoltyansky <kazohelmet@gmail.com>
2023-03-19 11:06:20 +0200

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const api = process.env === "production" ? "" : "http://localhost:5000";
+
   const [backendData, setBackendData] = useState([{}]);
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("/api");
+      const response = await fetch(`${api}/api`);
       const data = await response.json();
       console.log(data);
       setBackendData(data);
     };
     fetchData();
-  }, []);
+  }, [api]);
 
   return (
     <div>
       <h1>Frontend</h1>
       <h2>Backend Data</h2>
-      {/* <ul>
+      <ul>
         {backendData.users.map((user) => (
           <li key={user}>{user}</li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
